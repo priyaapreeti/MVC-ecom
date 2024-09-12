c29081531e565d324b1d432bef3064d1909c2242
codesofpreeti <preetiipriyaa@gmail.com>
2024-09-12 09:43:41 +0530

    error logging using winston library

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,21 +1,30 @@
 //file system
+import { log } from "console";
 import fs from "fs";
 
 //winston
+import winston from "winston";
 
 const fsPromise= fs.promises;
 
-function log(logData){
-    try {
-        logData=`${new Date().toString()} - ${logData}`;
-        fsPromise.appendFile('log.txt',logData);
-    } catch (error) {
-        console.log(error);
-    }
-}
+// function log(logData){
+//     try {
+//         logData=`${new Date().toString()} - ${logData}`;
+//         fsPromise.appendFile('log.txt',logData);
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+const winstonLogger=winston.createLogger({
+    level:"info",
+    format: winston.format.json(),
+    transports:[new winston.transports.File({filename: "filename.txt"})]
+})
 const loggerMiddleware= async(req,res,next)=>{
-    const logData=`${req.url} - ${JSON.stringify(req.body)} \n`;
-    await log(logData);
+    const logData=`${new Date().toString()} - ${req.url} - ${JSON.stringify(req.body)}`;
+    // await log(logData);
+    winstonLogger.info(logData);
     next();
 }
 export default loggerMiddleware;
